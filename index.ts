52df9f84515c8bdf8b02e99ffb106c9dccbbda14
mhdpasha <pashaalbara01@gmail.com>
2024-04-19 12:49:32 +0700

    idk dis

--- index.ts
+++ index.ts
@@ -1,8 +1,23 @@
+import { API_KEYS } from "./key";
+
 const server = Bun.serve({
-    port: 3000,
-    fetch(req) {
-      return new Response("Bun!");
-    },
-  });
-  
-  console.log(`Listening on http://localhost:${server.port} ...`);
+  port: 3000,
+  fetch(req) {
+    const apiKey = req.headers.get("Authorization");
+
+    if (apiKey === null || !Object.values(API_KEYS).includes(apiKey)) {
+      return new Response("Unauthorized", { status: 401 });
+    }
+
+    if (!(req.method === "GET" && req.url === "data")) {
+      return new Response("Not Found", { status: 404 });
+    }
+
+    const data = { message: "Hello, world!", timestamp: Date.now() };
+    return new Response(JSON.stringify(data), {
+      headers: { "Content-Type": "application/json" },
+    });
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port}`);
---
+++ key.ts
@@ -0,0 +1,5 @@
+export const API_KEYS = {
+    key1: "API_KEY_1",
+    key2: "API_KEY_2",
+    key3: "API_KEY_3",
+}
